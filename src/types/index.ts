0fac7c8dd38523c56473d2a4279e1d3746ada8b5
zbruxin-ctrl <zbruxin@gmail.com>
2026-05-12 13:48:48 -0300

    feat: add yopmail to EmailProvider in Config types

--- src/types/index.ts
+++ src/types/index.ts
@@ -19,7 +19,7 @@ export interface ProxyConfig {
   password?: string;
 }
 
-export type EmailProvider = 'temp-mail.io' | 'mail.tm';
+export type EmailProvider = 'temp-mail.io' | 'mail.tm' | 'yopmail';
 
 export interface Config {
   cadastroUrl: string;
