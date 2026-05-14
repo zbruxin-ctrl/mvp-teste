c2b384756bba17c8c9aa9e3d7b845f12e3650560
zbruxin-ctrl <zbruxin@gmail.com>
2026-05-14 19:53:39 -0300

    refactor: remove speedHelper.ts duplicado (funções já existem em humanActions.ts)

--- src/playwright/speedHelper.ts
+++
@@ -1,9 +0,0 @@
-import { globalState } from '../state/globalState';
-
-export function isSpeedMode(): boolean {
-  return !!(globalState.getState().config as any)?.speedMode;
-}
-
-export function sp(normal: number): number {
-  return isSpeedMode() ? Math.max(30, Math.round(normal * 0.4)) : normal;
-}
